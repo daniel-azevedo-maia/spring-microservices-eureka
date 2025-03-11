8fc3e33a4c11a8652371102b0805018feca3a45f
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2025-03-11 18:27:37 -0300

    avancos nos endpoints

--- order-service/src/main/java/com/ms/order/controller/OrderController.java
+++ order-service/src/main/java/com/ms/order/controller/OrderController.java
@@ -1,42 +1,32 @@
 package com.ms.order.controller;
 
 import com.ms.order.client.ProductClient;
-import com.ms.order.dto.OrderDTO;
 import com.ms.order.dto.ProductDTO;
-import com.ms.order.entity.Order;
-import com.ms.order.service.OrderService;
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/orders")
 public class OrderController {
 
-    private final OrderService service;
     private final ProductClient productClient;
 
-    @GetMapping("/test/product/{id}")
-    public ProductDTO testProductClient(@PathVariable("id") Long id) {
-        return productClient.getProductById(id);
-    }
-
     @PostMapping
-    public ResponseEntity<Order> create(@RequestBody @Valid OrderDTO dto) {
-        return ResponseEntity.ok(service.createOrder(dto));
+    public ResponseEntity<String> createOrder(@RequestParam Long productId) {
+
+        ProductDTO product = productClient.getProductById(productId);
+
+        if(product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
+        }
+
+        // Aqui você poderia persistir o pedido, mas por enquanto vamos apenas simular:
+        return ResponseEntity.ok("Pedido criado com produto: " + product.getName() + ", valor: R$" + product.getPrice());
+
     }
 
-    @GetMapping
-    public ResponseEntity<List<Order>> listAll() {
-        return ResponseEntity.ok(service.listAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.findById(id));
-    }
 }
